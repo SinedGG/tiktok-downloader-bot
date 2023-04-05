6cc3454d3897612db8ad02dbe2782b9bb6085b66
Sined <denis2320032@gmail.com>
2023-04-05 16:30:58 +0300

    🍃 | Added message redirect

--- modules/separator.js
+++ modules/separator.js
@@ -10,17 +10,31 @@ module.exports = async (ctx) => {
 
     var url;
 
-    if (text.includes(`youtube.com/shorts`)) return await shotrs(ctx);
+    if (text.includes(`youtube.com/shorts`)) await shotrs(ctx);
 
     if (text.includes(`tiktok.com`)) url = await tiktok(text);
     if (text.includes(`instagram.com/reel`)) url = await insta(text);
 
+    if (!url) return;
+
     await wait(500);
 
-    await bot.telegram.sendVideo(ctx.chat.id, url, {
-      reply_to_message_id: ctx.message.message_id,
-      disable_notification: true,
-    });
+    if (
+      ctx.chat.id == process.env.MAIN_CHANNEL ||
+      ctx.chat.id == process.env.MEDIA_CHANNEL
+    ) {
+      var username = ctx.from.username;
+      if (!username) username = ctx.from.first_name;
+      else username = `@${username}`;
+      await bot.telegram.sendVideo(process.env.MEDIA_CHANNEL, url, {
+        caption: `Надіслано - ${username} \n${text}`,
+        disable_notification: true,
+      });
+    } else
+      await bot.telegram.sendVideo(ctx.chat.id, url, {
+        reply_to_message_id: ctx.message.message_id,
+        disable_notification: true,
+      });
   } catch (error) {
     console.log(error);
   }
